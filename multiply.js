2db599cfaff1c6975aef0642b447d5e9123e7d71
m.khizer <muhammadkhizzer1312@gmail.com>
2022-09-10 03:58:39 -0700

    initial commit

---
+++ multiply.js
@@ -0,0 +1,7 @@
+const multiply = (a , b) =>{
+
+    let total =  a * b
+
+    console.log('The product is ' + total)
+
+}
---
+++ sum.js
@@ -0,0 +1,7 @@
+const sum = (a , b) =>{
+
+    let total =  a + b
+
+    console.log('The sum is ' + total)
+
+}
